3b00b9b9657b93b0337092997d249ce5980eadfa
Marcin Radoszewski <moriturius@gmail.com>
2016-09-03 14:15:00 +0000

    Changed namespace and project name

--- src/fejmetr/web.clj
+++ src/fejmetr/web.clj
@@ -1,4 +1,4 @@
-(ns clojure-getting-started.web
+(ns fejmetr.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
@@ -9,7 +9,7 @@
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Heroku"})
+   :body "Hello from Heroku!"})
 
 (defroutes app
   (GET "/" []
--- test/fejmetr/web_test.clj
+++ test/fejmetr/web_test.clj
@@ -1,6 +1,6 @@
-(ns clojure-getting-started.web-test
+(ns fejmetr.web-test
   (:require [clojure.test :refer :all]
-            [clojure-getting-started.web :refer :all]))
+            [fejmetr.web :refer :all]))
 
 (deftest first-test
   (is false "Tests should be written"))
